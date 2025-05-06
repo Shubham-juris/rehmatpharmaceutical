1dcc6ed6089facbe3d81efaca492ea96de9523fc
vishantjurish <vishant.juris@gmail.com>
2025-05-06 11:27:29 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline text-center py-56 bg-amber-400">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
