967d59952b104fe95b907eeb488063df4b03bb27
Vinay730 <vinay.juris@gmail.com>
2025-05-07 12:01:13 +0100

    contact route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import Homepage from '../pages/Homepage/Homepage';
 import AboutUsPage from '../pages/Aboutuspage/AboutUsPage';
 import ServicePage from '../pages/Service/ServicePage';
+import ContactUsPage from "../pages/ContactUS/ContactUsPage";
 
 const Router = () => {
   return (
@@ -11,6 +12,8 @@ const Router = () => {
         <Route path='/' element={<Homepage/>}/>
         <Route path='/aboutUs' element={<AboutUsPage/>}/>
         <Route path='/service' element={<ServicePage/>}/>
+        <Route path='/contact' element={<ContactUsPage/>}/>
+
       </Routes>
     </>
   )
